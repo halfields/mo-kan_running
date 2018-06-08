85a82a0ac39d88b7430b85cd02ea3dbb74b11dbe
halfields <hfields697@aol.com>
2018-06-08 14:11:25 -0500

    add organizers and events migrations

---
+++ db/migrate/20180608184450_create_organizers.rb
@@ -0,0 +1,9 @@
+class CreateOrganizers < ActiveRecord::Migration[5.1]
+  def change
+  	create_table :organizers do |t| 
+  		t.string :name
+  		t.string :email
+  		t.string :password
+  	end
+  end
+end
