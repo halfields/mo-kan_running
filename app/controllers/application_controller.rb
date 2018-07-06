220e5c935e2f886a5f1372693c967de150d6b60d
halfields <hfields697@aol.com>
2018-07-06 18:40:20 -0500

    added welcome method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < Sinatra::Base
 	enable :sessions
 	set :session_secret, "hals_secret"
 	set :views, Proc.new {File.join(root, "../views/")}
+
+	get '/' do #load welcome page
+		erb :welcome
+	end
 end
