53f8a8ae5c8e152a002f7b83a04fe4f65f9b83f1
halfields <hfields697@aol.com>
2018-07-08 00:13:54 -0500

    add signup actions to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,17 @@ class ApplicationController < Sinatra::Base
 	get '/' do #load welcome page
 		erb :welcome
 	end
+
+	get '/signup' do # load signup form
+		erb :signup
+	end
+
+	post '/signup' do # signup 
+		if params[:email] == "" || params[:password] 
+			flash[:alert] "Need email and password fields filled out."
+			erb :signup
+		else
+			Organizer.create(email: params[:email], password: params[:password])
+			redirect '/login'
+		end
 end
