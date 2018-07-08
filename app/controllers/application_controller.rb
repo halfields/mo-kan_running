c10e9289afe1ce25005203778a7982a356ab99a2
halfields <hfields697@aol.com>
2018-07-08 00:31:05 -0500

    added bcrypt and rack-flash3 gems to Gemfile and added login actions to appllication_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,24 @@ class ApplicationController < Sinatra::Base
 	post '/signup' do # signup 
 		if params[:email] == "" || params[:password] 
 			flash[:alert] "Need email and password fields filled out."
-			erb :signup
+			render :signup
 		else
 			Organizer.create(email: params[:email], password: params[:password])
 			redirect '/login'
 		end
+	end
+
+	get '/login' do #load login form
+		erb :login
+	end
+
+	post '/login' do #login
+		@organizer = Organizer.find_by(email: params[:email])
+		if @organizer && @organizer.authenticate(params[:password])
+			session[:organizer_id] = @organizer.id		
+		else
+			flash[:alert] "Login failed. Please try again."
+			render :login
+		end
+	end
 end
