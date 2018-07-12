b9b4aa8cc475c1417795c88c71c8e990bd11cea9
halfields <hfields697@aol.com>
2018-07-12 00:05:11 -0500

    changes to post '/events' and patch '/events/:id' in events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,22 +21,34 @@ class EventsController < ApplicationController
 
 	post '/events' do #creates an event
 		@event = Event.create(params)
+		@races = []
+		@races << "Fun Run" if params[:fun_run]
+		@races << "5K" if params[:five_k]
+		@races << "10K" if params[:ten_k]
+		@races << "Half Marathon" if params[:half_marathon]
+		@races << "Marathon" if params[:marathon]
+		@races << "Ultra" if params[:ultra]
 		redirect_to "/events/#{@event.id}"
 	end
 
 	patch '/events/:id' do #updates an event
 		@event = Event.find(params[:id])
-		@event.name = params[:name]
-		@event.date = params[:date]
-		@event.contact_info = params[:contact_info]
-		@event.message = params[:message]
-		@event.fun_run = params[:fun_run]
-		@event.five_k = params[:five_k]
-		@event.ten_k = params[:ten_k]
-		@event.half_marathon = params[:half_marathon]
-		@event.ultra = params[:ultra]
-		@event.update
-		redirect_to "/event/#{@event.id}"
+		if current_organizer && @event.organizer_id == current_organizer.id
+			@event.name = params[:name]
+			@event.date = params[:date]
+			@event.contact_info = params[:contact_info]
+			@event.message = params[:message]
+			@event.fun_run = params[:fun_run]
+			@event.five_k = params[:five_k]
+			@event.ten_k = params[:ten_k]
+			@event.half_marathon = params[:half_marathon]
+			@event.marathon = params[:marathon]
+			@event.ultra = params[:ultra]
+			@event.update
+			redirect_to "/event/#{@event.id}" 
+		else
+			flash[:message] = "You are not authorized to edit this event"
+		end
 	end
 
 	delete '/events/:id' do #delete anevent
@@ -44,6 +56,5 @@ class EventsController < ApplicationController
 		@event.delete
 		redirect_to "/events/index"
 	end
-
 
 end
