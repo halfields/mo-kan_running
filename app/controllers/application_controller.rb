83ef1bcfff2e5cb0fca0698b1f7eaace327a5431
halfields <hfields697@aol.com>
2018-07-11 23:59:17 -0500

    mounted rack::flash, changes to post'/signup' and post'/login, and added get '/logput'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < Sinatra::Base
 	enable :sessions
 	set :session_secret, "hals_secret"
 	set :views, Proc.new {File.join(root, "../views/")}
+	use rack::flash
 
 	get '/' do #load welcome page
 		erb :welcome
@@ -12,40 +13,51 @@ class ApplicationController < Sinatra::Base
 		erb :signup
 	end
 
-	post '/signup' do # signup 
-		if params[:email] == "" || params[:password] 
-			flash[:alert] = "Need email and password fields filled out."
-			render :signup
+	post '/signup' do #signup
+		if params[:email] == "" || params[:password] == ""
+			flash[:error] = "Both email and password needed for signup"
+			erb :signup
 		else
-			Organizer.create(email: params[:email], password: params[:password])
-			redirect '/login'
-		end
-	end
-
-	get '/login' do #load login form
-		erb :login
-	end
-
-	post '/login' do #login
-		@organizer = Organizer.find_by(email: params[:email])
-		if @organizer && @organizer.authenticate(params[:password])
-			session[:organizer_id] = @organizer.id		
-		else
-			flash[:alert] = "Login failed. Please try again."
-			render :login
-		end
-	end
-
-	helpers do 
-
-		def logged_in?
-			!!session[:organizer_id]
+			@organizer = Organizer.create(params)
+			session[:organizer_id] = @organizer.id 
+			@events = @organizer.events
+			redirect "/organizers/#{@organizer.id}"
 		end
 
-		def current_user
-			Organizer.find(session[:organizer_id])
+		get "/login" do #load login form 
+			erb :login 
 		end
+
+	post "/login" # login 
+	  @organizer = Organizer.find_by(email: params[:email])
+	  if @organizer && @organizer.authenticate(params[:password])
+	  	session[:organizer_id] = @organizer.id
+	  	redirect "/events"
+	  else
+	  	flash[:error] = "Login error. Please try again"
+	  	erb :login
+	  end
+
+	  get "/logout" do # logout
+	  	session.clear
+	  	redirect "/"
+	  end
+
+  helpers do 
+
+  	def logged_in?
+  		!!session[:organizer_id]
+  	end
+
+  	def current_organizer
+  		Organizer.find(session[:organizer_id])
+  	end
+
+  	#def current_organizer
+  	#	@current_organizer ||= Organizer.find(session[:organizer_id]) if session[:organizer_id]
+  	#end
 
 	end
 
 end
+
