f7f4cf4ce69c689b388ae88069abae1d551fcb49
halfields <hfields697@aol.com>
2018-07-11 23:53:35 -0500

    added 'has_secure_password' macro to organizer model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ActiveRecord::Base
 	belongs_to :organizer
 
 	validates :name, :date, presence: true
+
 end
--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,5 +1,7 @@
 class Organizer < ActiveRecord::Base
 
+	has_secure_password
+
 	has_many :events
 
 	validates :name, :email, :password, presence: true
