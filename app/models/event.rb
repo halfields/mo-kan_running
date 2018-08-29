6cc6795ae73486f1d3fd795f4500406f0ffa6895
halfields <hfields697@aol.com>
2018-08-29 13:28:59 -0500

    add association to races, add RACES constant, add all_races method

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,13 +2,19 @@ class Event < ActiveRecord::Base
 
 	require 'date'
 
-	serialize :races, Array
 	belongs_to :organizer
+	has_and_belongs_to_many :races
 	validates :name, :date, presence: true
+
+	RACES = %w{fun_run five_k ten_k half_marathon marathon ultra}
 
 	def expired?
 		date = Date.strptime(self.date, "%Y%m%d")
 		Date.today > date
+	end
+
+	def all_races
+		RACES
 	end
 
 end
